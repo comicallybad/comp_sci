944fb12c489bfda0d5970adea4fe046de59cffbe
comicallybad <amp5646@yahoo.com>
2020-01-29 12:36:02 -0600

    pushed changes

--- ReadMe.md
+++ ReadMe.md
@@ -2,6 +2,7 @@ Repo For Comp Sci C++.
 1. Git clone
 2. Install extensions "run" and "c++" for VSCode (bottom button on left panel)
 3. Open Comp_Sci folder with VSCode
+4. Type "cd {folder name ex: Array Sorting or on mac: Array\ Sorting}"
 
 IF THE CODE DOES NOT TAKE USER INPUT: 
 1. Open the main.cpp file for the project ex: "main.cpp" in "Array Sorting"
--- Week 2 cin/main.cpp
+++ Week 2 cin/main.cpp
@@ -7,11 +7,24 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World" << endl;
-    string name = " ";
+    // cout << "Hello World" << endl;
+    // string name = " ";
 
-    cout << "Please enter your name: ";
-    getline(cin, name);
+    // cout << "Please enter your name: ";
+    // getline(cin, name);
 
-    cout << name << endl;
+    // cout << name << endl;
+
+    // cout << 5 * 5 * 5 * 5 * 5 << endl;
+    // cout << pow(5, 5) << endl;
+    // cout << pow(2, 8) << endl;
+
+    // cout << 10 + 4 << endl;
+
+    int x = 0;
+    cout << x << endl;
+    x = x + 1;
+    cout << x << endl;
+    x = x + 1;
+    cout << x << endl;
 }
