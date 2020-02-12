955625a7065f5b4ba4a3a4fdcbcc71c7776a95f2
comicallybad <amp5646@yahoo.com>
2020-02-11 18:58:44 -0600

    .

--- Random Projects/Array Sorting/sort.cpp
+++ Random Projects/Array Sorting/sort.cpp
@@ -3,12 +3,10 @@
 
 void Sort::arraySort()
 {
-    {
-        int temp;
-        for (int i = 0; i < sizeof(arrayList) / sizeof(arrayList[0]); i++)
-            for (int j = 0; j < (sizeof(arrayList) / sizeof(arrayList[0])) - 1 - i; j++)
-                arrayList[j] > arrayList[j + 1] ? temp = arrayList[j + 1], arrayList[j + 1] = arrayList[j], arrayList[j] = temp : arrayList[j + 1] = arrayList[j + 1];
-        for (int p = 0; p < sizeof(arrayList) / sizeof(arrayList[0]); p++)
-            p < sizeof(arrayList) / sizeof(arrayList[0]) - 1 ? cout << arrayList[p] << ", " : cout << arrayList[p] << endl;
-    };
+    int temp;
+    for (int i = 0; i < sizeof(arrayList) / sizeof(arrayList[0]); i++)
+        for (int j = 0; j < (sizeof(arrayList) / sizeof(arrayList[0])) - 1 - i; j++)
+            arrayList[j] > arrayList[j + 1] ? temp = arrayList[j + 1], arrayList[j + 1] = arrayList[j], arrayList[j] = temp : arrayList[j + 1] = arrayList[j + 1];
+    for (int p = 0; p < sizeof(arrayList) / sizeof(arrayList[0]); p++)
+        p < sizeof(arrayList) / sizeof(arrayList[0]) - 1 ? cout << arrayList[p] << ", " : cout << arrayList[p] << endl;
 };
