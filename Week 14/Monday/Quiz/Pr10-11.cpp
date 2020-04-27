cb8d8d23bd15c198b27f81b7123655d3f650ead6
amp5646@yahoo.com <amp5646@yahoo.com>
2020-04-27 12:04:01 -0500

    Updated Week 14 Monday Quiz

--- Week 14/Monday/Quiz/Pr10-11.cpp
+++ Week 14/Monday/Quiz/Pr10-11.cpp
@@ -21,10 +21,14 @@ int main()
 
    i.setLastName(input);
 
+   string upperFirst;
+   for (auto &c : i.getFirstName())
+      upperFirst += toupper(c);
+
    cout << "Your first name is: " << i.getFirstName() << endl;
    cout << "Your last name is: " << i.getLastName() << endl;
    cout << "Your name is: " << i.getLength(i.getFirstName(), i.getLastName()) << " characters long." << endl;
-   cout << "Your name in reverse order is: " << i.getLastName() << ", " << i.getFirstName() << endl;
+   cout << "Your name in reverse order is: " << i.getLastName() << ", " << upperFirst << endl;
 
    return 0;
 }
